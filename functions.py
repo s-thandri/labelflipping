e38cbfc9ea9650d1f9a08ac0d53ff03d5b31302a
Shashank Thandri <shashank.thandri@gmail.com>
2024-04-02 13:49:01 -0400

    testing functions

--- functions.py
+++ functions.py
@@ -55,12 +55,9 @@ def s_parity(test_data, predictors, prediction_df, sensitive_attr, concat_col):
 #independent_var: Column names
 #dependent_var: Prediction column name
 #concat_col: Name of column creating new DF on
-def logistic_regression(train_dataset, independent_var, dependent_var, concat_col):
+def logistic_regression(independent_var, concat_col,x_test,x_train,y_test,y_train):
         #Split the data up into train and test values and then run the DT model
         #These steps aren't neccessary to consistenly do over and over again
-        x = train_dataset[independent_var].values
-        y = train_dataset[dependent_var].values
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,random_state=310)
         clf = LogisticRegression(class_weight=None, max_iter=100)
         log = clf.fit(x_train,y_train)
         prediction = log.predict(x_test)
@@ -80,3 +77,112 @@ def logistic_regression(train_dataset, independent_var, dependent_var, concat_co
         predicted_df = pd.DataFrame(prediction, columns = [concat_col])
 
         return accuracy, test_demo_df, predicted_df
+
+#Import the German credit into a csv
+columns = ["checking_account_status", "duration", "credit_history", "purpose", "credit_amount", "savings_account_status", "employment_status", "installment_rate", "personal_status", "other_debtors", "residence_since", "property", "age", "other_installment_plans", "housing", "existing_credits", "job", "num_dependents", "telephone", "foreign_worker", "credit_risk"]
+german_df = pd.read_table('german_income/german.data', header = None, delim_whitespace=True, names = columns)
+
+#Encode the data
+train_full_df=pd.get_dummies(german_df,columns=['checking_account_status','credit_history','savings_account_status','employment_status','personal_status','other_debtors','property','other_installment_plans','housing','job','telephone','foreign_worker','purpose'], drop_first=True)
+
+
+#Initialize all the different columns including predictors and target variable
+target_var = 'credit_risk'
+predictors = ['duration', 'credit_amount', 'installment_rate',
+       'residence_since', 'age', 'existing_credits', 'num_dependents',
+       'checking_account_status_A12',
+       'checking_account_status_A13', 'checking_account_status_A14',
+       'credit_history_A31', 'credit_history_A32', 'credit_history_A33',
+       'credit_history_A34', 'savings_account_status_A62',
+       'savings_account_status_A63', 'savings_account_status_A64',
+       'savings_account_status_A65', 'employment_status_A72',
+       'employment_status_A73', 'employment_status_A74',
+       'employment_status_A75', 'personal_status_A92', 'personal_status_A93',
+       'personal_status_A94', 'other_debtors_A102', 'other_debtors_A103',
+       'property_A122', 'property_A123', 'property_A124',
+       'other_installment_plans_A142', 'other_installment_plans_A143',
+       'housing_A152', 'housing_A153', 'job_A172', 'job_A173', 'job_A174',
+       'telephone_A192', 'foreign_worker_A202']
+
+#X-train is all the data points for training
+#y_train contains the labels for each of the training data points
+#x_test contains all the testing data points
+#y_test contains the ground truth for each of the test data points
+
+#The idea for this is to only split the dataset once and to make predictions on it
+#Rather than splitting it everytime I call the functions
+
+x = train_full_df[predictors].values
+y = train_full_df[target_var].values
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=310)
+#Combine these to build the dataset I need to flip data on it 
+train_demo_df = pd.DataFrame(x_train, columns = predictors)
+train_outcome_df = pd.DataFrame(y_train, columns = ['credit_risk'])
+iterative_append = pd.concat([train_demo_df, train_outcome_df], axis=1)
+
+#When flipping the label make sure convert the flip before into its original value
+
+#Combine x_train and y_train
+#Retrain the model after combining them and then flip and calculate
+#Implement label flipping and recalculate
+
+#Each list holds a different value
+
+#list_parity holds each parity value after each flip and recalculation
+list_parity = []
+#list_acc holds the accuracy of each iteration after a flip
+list_acc = []
+#list_flip holds the row number of the flip; starts at 0 and goes through the whole
+list_flip = []
+
+#Iterating through the training dataset
+for index, row in iterative_append.iterrows():
+  #If the row that I am on has the label 1 (credit_risk is the label name) then I will flip it
+  #And then run the logistic_regression function to get the accuracy, the DF that contains datapoints,
+  #And the DF that contains the predictions
+  #The two DF's are then combined in the s_parity function to calculate parity
+    if row['credit_risk'] == 1:
+      #Flip the label
+        iterative_append.at[index, 'credit_risk'] = 2
+
+        #Run the logistic regression function
+        #train_full_df: training dataset
+        #predictors: IV's
+        #target_var: DV's
+        #'credit_risk12': this is the column that the labels for the test_prediction is in
+        accuracy,test_datapoints, test_prediction = logistic_regression(iterative_append ,'credit_risk_12',x_train,x_test,y_train,y_test)
+
+        #list_flip: contains the row number that has just been flipped
+        #list_num: contains the accuracy value just calculated
+        list_flip.append(index)
+        list_acc.append(accuracy)
+
+        ##################################################################################
+        #After calculating the accuracy parity calculation is next
+        parity = s_parity(test_datapoints, predictors, test_prediction, 'age', 45, 'credit_risk_12')
+        #list_parity: contains the parity value after the flip
+        #print(parity)
+        list_parity.append(parity)
+
+        #Flips the label back to its original value
+        iterative_append.at[index,'credit_risk'] = 1
+
+    #Repeats all the same steps as above but this time if the original label is 2
+    elif row['credit_risk'] == 2:
+        iterative_append.at[index, 'credit_risk'] = 1
+
+        accuracy,test_datapoints, test_prediction = logistic_regression(iterative_append ,'credit_risk_12',x_train,x_test,y_train,y_test)
+        list_flip.append(index)
+        list_acc.append(accuracy)
+
+        ##################################################################################
+        parity = s_parity(test_datapoints, predictors, test_prediction, 'age', 45, 'credit_risk_12')
+        #print(parity)
+        list_parity.append(parity)
+
+        iterative_append.at[index,'credit_risk'] = 2
+
+
+print(list_parity)
+
